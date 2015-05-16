44b3b98122a9d39620468115c2dceb541e7387a9
pearson-training <hanieel.rodriguez@pearson.com>
2015-05-15 22:50:41 -0400

    add jquery-1.11.3.min.js

--- fluent.conf.2015/js/scripts.js
+++ fluent.conf.2015/js/scripts.js
@@ -1,1 +1,90 @@
 // JavaScript Document
+
+jQuery(document).ready(function($){
+	
+	
+	 // General Lighbox close
+	  $('.lightbox_wrap').click(function() {
+		  $('.lightbox_wrap').hide();
+		  $(this).children('.lightbox').hide();
+		  $('body').removeClass('noscroll');
+	  });
+	  $('.lightbox').click(function(event) {
+		  event.stopPropagation();
+	  });
+	  
+	  
+	  
+	  //Scrollto function; Removes hash from href and scrolls to element ID
+    $(function(){
+        $('a.scrollto[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {             
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                    scrollTop: target.offset().top - 135 // Distance from top
+                    }, 1000);
+                    return false;
+                }           
+            }
+        });
+    });
+
+    //postion: sticky;  
+    /*
+    var stickyNavTop = $('header nav').offset().top;
+    var stickyNav = function(){  
+    var scrollTop = $(window).scrollTop();  
+        if (scrollTop > stickyNavTop) {   
+            $('header nav').addClass('sticky');
+        } else {  
+            $('header nav').removeClass('sticky');
+        }  
+    };  
+    stickyNav();  
+    $(window).scroll(function() {  
+        stickyNav();  
+    });
+    */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
